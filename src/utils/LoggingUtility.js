de8f57d726fa9e5473887f17063c3bdc3707c02d
mephmanx <52037738+mephmanx@users.noreply.github.com>
2024-08-09 16:25:37 +0000

    Deploying to release from @ dotcomrow/pulse-graphql@98e2a5b799f210c950153208e9bfb8211a64ad52 🚀

---
+++ src/utils/LoggingUtility.js
@@ -0,0 +1,17 @@
+import { GCPLogger } from "npm-gcp-logging";
+
+export default {
+  async logEntry(context, entries) {
+    var finalEntries = [];
+    for (var entry of entries) {
+      entry.spanId = context.spanId;
+      finalEntries.push(entry);
+    }
+    await GCPLogger.logEntry(
+      context.GCP_LOGGING_PROJECT_ID,
+      context.LOGGING_TOKEN,
+      context.LOG_NAME,
+      finalEntries
+    );
+  }
+}

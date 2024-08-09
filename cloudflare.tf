3693b529c72d1975af5e5629120afe210e2bd205
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-08-09 15:04:02 -0400

    updating config
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- cloudflare.tf
+++ cloudflare.tf
@@ -13,16 +13,6 @@ resource "cloudflare_worker_route" "project_route" {
   script_name = cloudflare_worker_script.project_script.name
 }
 
-resource "cloudflare_d1_database" "cache" {
-  account_id = var.cloudflare_account_id
-  name       = "${var.project_name}_database"
-}
-
-resource "cloudflare_workers_kv_namespace" "settings" {
-  account_id = var.cloudflare_account_id
-  title      = "${var.project_name}-settings"
-}
-
 resource "cloudflare_worker_script" "project_script" {
   account_id         = var.cloudflare_account_id
   name               = var.project_name
@@ -30,19 +20,9 @@ resource "cloudflare_worker_script" "project_script" {
   compatibility_date = "2023-08-28"
   module             = true
 
-  kv_namespace_binding {
-    name         = "SETTINGS"
-    namespace_id = cloudflare_workers_kv_namespace.settings.id
-  }
-
   plain_text_binding {
     name = "CORS_DOMAINS"
     text = var.ALLOWED_HOSTS
-  }
-
-  plain_text_binding {
-    name = "DOMAIN"
-    text = var.domain
   }
 
   plain_text_binding {
@@ -59,6 +39,16 @@ resource "cloudflare_worker_script" "project_script" {
     text = "${var.project_name}_worker_log"
   }
 
+  plain_text_binding {
+    name = "PULSE_DATABASE_PROJECT_ID"
+    text = "pulsedb_dataset"
+  }
+
+  plain_text_binding {
+    name = "PULSE_DATASET"
+    text = "pulsedb-16e0922db52092e4"
+  }
+
   secret_text_binding {
     name = "GCP_LOGGING_CREDENTIALS"
     text = var.GCP_LOGGING_CREDENTIALS
@@ -72,30 +62,5 @@ resource "cloudflare_worker_script" "project_script" {
   secret_text_binding {
     name = "GCP_USERINFO_CREDENTIALS"
     text = var.GCP_USERINFO_CREDENTIALS
-  }
-
-  secret_text_binding {
-    name = "GLOBAL_SHARED_SECRET"
-    text = var.GLOBAL_SHARED_SECRET
-  }
-
-  service_binding {
-    name    = "nodejs-cloudflare-logging-service"
-    service = "nodejs-cloudflare-logging-service"
-  }
-
-  service_binding {
-    name    = "nodejs-cloudflare-service-template"
-    service = "nodejs-cloudflare-service-template"
-  }
-
-  service_binding {
-    name    = "nodejs-cloudflare-results-service"
-    service = "nodejs-cloudflare-results-service"
-  }
-
-  d1_database_binding {
-    name        = "cache"
-    database_id = cloudflare_d1_database.cache.id
   }
 }

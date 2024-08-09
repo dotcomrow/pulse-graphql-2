a93eb92d0da7b4dbabe7f4af77b8d226ff4cd8b4
mephmanx <52037738+mephmanx@users.noreply.github.com>
2024-08-09 19:20:59 +0000

    Deploying to release from @ dotcomrow/pulse-graphql@16de39e0bc35c63e5eecf823d3dda48e9155109d 🚀

---
+++ src/index.js
@@ -0,0 +1,75 @@
+import { createYoga, createSchema } from "graphql-yoga";
+import { serializeError } from "serialize-error";
+import resolvers from "./resolvers/resolvers.js";
+import loadFileFromBucket from "./schema/loadFileFromBucket.js";
+import { buildClientSchema, printSchema } from "graphql";
+import { makeExecutableSchema } from "graphql-tools";
+import { GCPAccessToken } from "npm-gcp-token";
+import { default as AuthenticationUtility } from "./utils/AuthenticationUtility.js";
+import { default as LogUtility } from "./utils/LoggingUtility.js";
+
+var schema = undefined;
+var yoga = undefined;
+
+export default {
+  async fetch(request, env, ctx) {
+    self.location = new URL("https://www.google.com");
+
+    var logging_token = (await new GCPAccessToken(env.GCP_LOGGING_CREDENTIALS).getAccessToken("https://www.googleapis.com/auth/logging.write")).access_token;
+    var database_token = (await new GCPAccessToken(env.GCP_BIGQUERY_CREDENTIALS).getAccessToken("https://www.googleapis.com/auth/bigquery")).access_token;
+
+    var yoga_ctx = Object.assign({}, env);
+    yoga_ctx['DATABASE_TOKEN'] = database_token;
+    yoga_ctx['LOGGING_TOKEN'] = logging_token;
+    if (request.headers.get("Authorization"))
+      yoga_ctx['account'] = await AuthenticationUtility.fetchAccountInfo(request.headers.get("Authorization").split(" ")[1]);
+
+    if (!request.headers.get("SpanId"))
+      yoga_ctx['SpanId'] = crypto.randomUUID();
+    
+    if (!schema) {
+      var schemaString = await loadFileFromBucket(env, "graphql_schema.json");
+      var schemaObj = buildClientSchema(JSON.parse(schemaString));
+      var sdlString = printSchema(schemaObj);
+
+      schema = makeExecutableSchema({
+        typeDefs: sdlString,
+        resolvers: resolvers.resolvers,
+        context: env,
+      });
+    }
+
+    if (!yoga) {
+      yoga = createYoga({
+        schema,
+        context: yoga_ctx,
+        logging: "debug",
+        cors: {
+          origin: env.CORS_DOMAINS,
+          credentials: true,
+          methods: ["POST"],
+        },
+      });
+    }
+
+    try {
+      return yoga(request, yoga_ctx);
+    } catch (e) {
+      const responseError = serializeError(e);
+      await LogUtility.logEntry(yoga_ctx, [
+        {
+          severity: "ERROR",
+          jsonPayload: {
+            responseError,
+          },
+        },
+      ]);
+      return new Response(JSON.stringify(responseError), {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    }
+  },
+};

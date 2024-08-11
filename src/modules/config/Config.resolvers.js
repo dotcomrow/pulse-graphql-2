ed19b29b900f06db2eca40f17544001975da293d
mephmanx <52037738+mephmanx@users.noreply.github.com>
2024-08-11 14:25:52 +0000

    Deploying to release from @ dotcomrow/pulse-graphql@912cb56a893a361d5030e9bbe33da90562862338 🚀

---
+++ src/modules/config/Config.resolvers.js
@@ -0,0 +1,58 @@
+import { GCPBigquery } from "npm-gcp-bigquery";
+import { serializeError } from "serialize-error";
+import { default as LogUtility } from "../../utils/LoggingUtility.js";
+import { default as SQL } from "./SQL.js";
+
+export default {
+  Query: {
+    getAllConfig: async (parent, args, context) => {
+      try {
+        return await GCPBigquery.query(
+            context.PULSE_DATABASE_PROJECT_ID,
+            context.DATABASE_TOKEN,
+            SQL.all_config_query_sql(context)
+        );
+      } catch (error) {
+        const responseError = serializeError(error);
+        await LogUtility.logEntry(context, [
+          {
+            severity: "ERROR",
+            jsonPayload: {
+              responseError,
+            },
+          },
+        ]);
+      }
+    },
+    getConfigByName: async (parent, args, context) => {
+      try {
+        return await GCPBigquery.query(
+            context.PULSE_DATABASE_PROJECT_ID,
+            context.DATABASE_TOKEN,
+            SQL.config_by_name_query_sql(context, args.config_name)
+        );
+      } catch (error) {
+        const responseError = serializeError(error);
+        await LogUtility.logEntry(context, [
+          {
+            severity: "ERROR",
+            jsonPayload: {
+              responseError,
+            },
+          },
+        ]);
+      }
+    },
+  },
+  Config : {
+    config_value: async (parent) => {
+      return parent.config_value;
+    },
+    config_name: async (parent) => {
+      return parent.config_name;
+    },
+    updatedAt: async (parent) => {
+      return parent.updated_at;
+    },
+  }
+};

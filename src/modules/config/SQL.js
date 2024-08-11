5ecfe5420dc9196792e8296506d121da110a5357
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-08-11 10:35:47 -0400

    fix
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- src/modules/config/SQL.js
+++ src/modules/config/SQL.js
@@ -4,7 +4,7 @@ export default {
           "UNIX_MILLIS(updated_at) as updatedAt" +
           " from `" + context.PULSE_DATASET + ".configuration`";
     },
-    config_by_name_query_sql : (context, id, name) => {
+    config_by_name_query_sql : (context, name) => {
         return "SELECT config_name, config_value," +
           "UNIX_MILLIS(updated_at) as updatedAt" +
           " from `" + context.PULSE_DATASET + ".configuration` where config_name = '" + name + "'";

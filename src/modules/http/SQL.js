0c144aca72550851d95869030e92c7a13d8fb7ea
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-08-18 06:19:42 -0400

    updating resolver
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- src/modules/http/SQL.js
+++ src/modules/http/SQL.js
@@ -15,6 +15,21 @@ export default {
             request.schedule + 
             "', CURRENT_TIMESTAMP())";
     },
+    update_request_sql : (context, request) => {
+        return "UPDATE `" + context.PULSE_DATASET + ".requests` " +
+        "SET " +
+            "request_url = '" + request.request_url + 
+            "', request_method = '" + request.request_method + 
+            "', request_headers = JSON '" + JSON.stringify(request.request_headers) + 
+            "', request_query = JSON '" + JSON.stringify(request.request_query) +
+            "', request_body = JSON '" + JSON.stringify(request.request_body) +
+            "', schedule = '" + request.schedule + 
+            "', updated_at = CURRENT_TIMESTAMP() " +
+        "WHERE request_id = '" + request.request_id + "'";
+    },
+    delete_request_sql : (context, request_id) => {
+        return "DELETE FROM `" + context.PULSE_DATASET + ".requests` WHERE request_id = '" + request_id + "'";
+    },
     request_query_sql : (context, account_id) => {
         return "SELECT * FROM `" + context.PULSE_DATASET + ".requests` WHERE account_id = '" + account_id + "'";
     },

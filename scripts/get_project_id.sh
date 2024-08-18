8db87a9f9a9acf1040e49e3dad29350cd2cc4a0d
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-08-18 09:18:48 -0400

    maybe fix
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- scripts/get_project_id.sh
+++ scripts/get_project_id.sh
@@ -4,11 +4,9 @@ tar -xf google-cloud-cli-linux-x86_64.tar.gz
 
 ./google-cloud-sdk/install.sh
 
-echo $GOOGLE_CREDENTIALS > service-account.json
+printf '%s' "$GOOGLE_CREDENTIALS" > key.json
 
-cat service-account.json
-
-./google-cloud-sdk/bin/gcloud auth activate-service-account --key-file=service-account.json
+./google-cloud-sdk/bin/gcloud auth activate-service-account --key-file=key.json
 
 
 cat `gcloud projects list --format json --filter name="pulsedb-dev" | jq -r '.[0].projectId'` > project_id

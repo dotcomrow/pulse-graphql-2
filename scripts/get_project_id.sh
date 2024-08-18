d86066ed8072ce044d1286818e08983e878a9dd5
mephmanx <52037738+mephmanx@users.noreply.github.com>
2024-08-18 14:30:02 +0000

    Deploying to release from @ dotcomrow/pulse-graphql@57b290c2b9e60d0ea332db734c1baeeb432692ee 🚀

---
+++ scripts/get_project_id.sh
@@ -0,0 +1,13 @@
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-linux-x86_64.tar.gz
+
+tar -xf google-cloud-cli-linux-x86_64.tar.gz
+
+./google-cloud-sdk/install.sh
+
+printf '%s' "$GOOGLE_CREDENTIALS" > key.json
+
+./google-cloud-sdk/bin/gcloud auth activate-service-account --key-file=key.json
+
+PROJECT_ID=$(./google-cloud-sdk/bin/gcloud projects list --format json --filter name="${project}" | jq -r '.[0].projectId')
+
+echo $PROJECT_ID | tr -d ' ' | tr -dc '[[:print:]]' > project_id

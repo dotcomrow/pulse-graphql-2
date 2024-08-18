fcda04955e08e67b03490fad2066b6abff0f4928
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-08-18 10:50:31 -0400

    Dev (#29)
    * logging project id
    
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>
    
    * removing spaces
    
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>
    
    * try again?
    
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>
    
    * hmm
    
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>
    
    * another try
    
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>
    
    * another try
    
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>
    
    * whoops
    
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>
    
    * hmmm
    
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- scripts/get_project_id.sh
+++ scripts/get_project_id.sh
@@ -8,4 +8,8 @@ printf '%s' "$GOOGLE_CREDENTIALS" > key.json
 
 ./google-cloud-sdk/bin/gcloud auth activate-service-account --key-file=key.json
 
-echo $(./google-cloud-sdk/bin/gcloud projects list --format json --filter name="${project}" | jq -r '.[0].projectId') > project_id
+PROJECT_ID=$(./google-cloud-sdk/bin/gcloud projects list --format json --filter name="${project}" | jq -r '.[0].projectId')
+
+echo $PROJECT_ID > project_id_tmp
+
+tr -cd '[:print:]' < project_id_tmp > project_id

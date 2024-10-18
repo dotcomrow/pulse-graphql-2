c16ee8b292ab0bba1b2ff4fdb1329e08fd8079b4
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-10-17 22:46:45 -0400

    fixing names
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- src/resolvers/resolvers.js
+++ src/resolvers/resolvers.js
@@ -6,7 +6,7 @@ import * as Config from '../modules/config/Config.resolvers.js'
 import * as Http from '../modules/http/Http.resolvers.js'
 import * as CMS from '../modules/cms/CMS.resolvers.js'
 import * as FetchPictures from '../modules/pictures/fetch-requests/fetch-pictures.resolvers.js'
-import * as SavePictures from '../modules/pictures/save-requests/save-pictures.resolvers.js'
+import * as ModifyPictures from '../modules/pictures/modify-requests/modify-pictures.resolvers.js'
 
 var returnResolvers = [];
 
@@ -16,7 +16,7 @@ returnResolvers.push(Config.default);
 returnResolvers.push(Http.default);
 returnResolvers.push(CMS.default);
 returnResolvers.push(FetchPictures.default);
-returnResolvers.push(SavePictures.default);
+returnResolvers.push(ModifyPictures.default);
 
 const resolvers = mergeResolvers(returnResolvers);
 

a85ad3e8e2ee877d8389be1933bf24d6ae24d289
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-10-17 22:33:14 -0400

    adding picture request base
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- src/resolvers/resolvers.js
+++ src/resolvers/resolvers.js
@@ -5,6 +5,8 @@ import * as User from '../modules/user/User.resolvers.js'
 import * as Config from '../modules/config/Config.resolvers.js'
 import * as Http from '../modules/http/Http.resolvers.js'
 import * as CMS from '../modules/cms/CMS.resolvers.js'
+import * as FetchPictures from '../modules/pictures/fetch-requests/fetch-pictures.resolvers.js'
+import * as SavePictures from '../modules/pictures/save-requests/save-pictures.resolvers.js'
 
 var returnResolvers = [];
 
@@ -13,6 +15,8 @@ returnResolvers.push(User.default);
 returnResolvers.push(Config.default);
 returnResolvers.push(Http.default);
 returnResolvers.push(CMS.default);
+returnResolvers.push(FetchPictures.default);
+returnResolvers.push(SavePictures.default);
 
 const resolvers = mergeResolvers(returnResolvers);
 

535ec2df57ee50ce1c4ed0ef48ae9f3e80e714ee
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-10-25 12:37:14 -0400

    fixing query
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- src/modules/pictures/fetch-requests/fetch-pictures.resolvers.js
+++ src/modules/pictures/fetch-requests/fetch-pictures.resolvers.js
@@ -109,13 +109,18 @@ export default {
     },
     fetchPictureRequestsByBoundingBox: async (parent, args, context) => {
       try {
+        const min_latitude = args.min_latitude;
+        const min_longitude = args.min_longitude;
+        const max_latitude = args.max_latitude;
+        const max_longitude = args.max_longitude;
+        const bbox = min_latitude + " " + min_longitude + "," + max_latitude + " " + min_longitude + "," + max_latitude + " " + max_longitude + "," + min_latitude + " " + max_longitude + "," + min_latitude + " " + min_longitude;
         await LogUtility.logEntry(context, [
           {
             severity: "DEBUG",
             jsonPayload: {
               sql: SQL.fetch_picture_requests_within_bbox_sql(
                 context,
-                args.bbox
+                bbox
               ),
               message: "Request by bounding box query executing",
             },
@@ -127,7 +132,7 @@ export default {
           context.DATABASE_TOKEN,
           SQL.fetch_picture_requests_within_bbox_sql(
             context,
-            args.bbox
+            bbox
           )
         );
 

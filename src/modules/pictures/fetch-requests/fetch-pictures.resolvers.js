f323af1f1ae33182009916698386755debfab5bc
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-10-26 09:37:46 -0400

    changing to bbox
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- src/modules/pictures/fetch-requests/fetch-pictures.resolvers.js
+++ src/modules/pictures/fetch-requests/fetch-pictures.resolvers.js
@@ -109,18 +109,19 @@ export default {
     },
     fetchPictureRequestsByBoundingBox: async (parent, args, context) => {
       try {
-        const min_latitude = args.min_latitude;
-        const min_longitude = args.min_longitude;
-        const max_latitude = args.max_latitude;
-        const max_longitude = args.max_longitude;
-        const bbox = min_latitude + " " + min_longitude + "," + max_latitude + " " + min_longitude + "," + max_latitude + " " + max_longitude + "," + min_latitude + " " + max_longitude + "," + min_latitude + " " + min_longitude;
+        // const min_latitude = args.min_latitude;
+        // const min_longitude = args.min_longitude;
+        // const max_latitude = args.max_latitude;
+        // const max_longitude = args.max_longitude;
+        // const bbox = min_latitude + " " + min_longitude + "," + max_latitude + " " + min_longitude + "," + max_latitude + " " + max_longitude + "," + min_latitude + " " + max_longitude + "," + min_latitude + " " + min_longitude;
+        console.log(args.bbox);
         await LogUtility.logEntry(context, [
           {
             severity: "DEBUG",
             jsonPayload: {
               sql: SQL.fetch_picture_requests_within_bbox_sql(
                 context,
-                bbox
+                args.bbox
               ),
               message: "Request by bounding box query executing",
             },
@@ -132,7 +133,7 @@ export default {
           context.DATABASE_TOKEN,
           SQL.fetch_picture_requests_within_bbox_sql(
             context,
-            bbox
+            args.bbox
           )
         );
 

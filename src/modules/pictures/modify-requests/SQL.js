8987b46e5db8b7376380445ac1c1c3a7f355895b
mephmanx <52037738+mephmanx@users.noreply.github.com>
2024-10-28 00:19:24 +0000

    Deploying to release-dev from @ dotcomrow/pulse-graphql@a537d4df3763f232370175f3b2ddcfaab2c0bdfc 🚀

---
+++ src/modules/pictures/modify-requests/SQL.js
@@ -0,0 +1,26 @@
+export default {
+    save_picture_request_sql: (context, request, account_id, request_id) => {
+        return `INSERT INTO ${context.PULSE_DATASET}.picture_requests 
+                (account_id, 
+                request_id, 
+                location, 
+                direction,
+                updated_at,
+                capture_timestamp,
+                requestTitle,
+                requestDescription,
+                bidType) 
+                VALUES 
+                (
+                    '${request.account_id}', 
+                    '${request.request_id}', 
+                    ST_GEOGFROMGEOJSON("${request.location}"), 
+                    ${request.direction},
+                    CURRENT_TIMESTAMP(),
+                    TIMESTAMP_MILLIS(${request.capture_timestamp}),
+                    '${request.requestTitle}',
+                    '${request.requestDescription}',
+                    '${request.bidType}'
+                )`;
+    }
+};
